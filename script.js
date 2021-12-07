74cc8ce872cafefb3be2a2c240fd4e156e0a4ea6
CosimaLagae <cosima.lagae@student.ehb.be>
2021-12-07 10:19:48 +0100

    map geolocation

---
+++ script.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29zaW1hcm9zaWUiLCJhIjoiY2t1d3BtYnF4MDZrcTJucXFoNGN1M3hkaCJ9.RgMCkP3mQoUKWG2O5Rw4Fw';
+
+navigator.geolocation.getCurrentPosition(succesLocation, errorLocation, {
+    enableHighAccuracy: true
+})
+
+function succesLocation(position){
+    console.log(position)        
+}
+
+function errorLocation(){
+
+}
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
