477df269201d152e15b1ff37cc209e6bdf6ad2a7
CosimaLagae <cosima.lagae@student.ehb.be>
2021-12-11 16:42:45 +0100

    install npm body-parser + POST request

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    console.log('local root called!');
+    //res.send("hello Cosi")
+    res.redirect('/info.html');
+})
+
+app.get('/test', (req, res)=>{
+    res.send('test succeeded!')
+})
+
+app.get('/data', (req, res)=>{
+    let exampleData = {
+        name: 'Cosi',
+        age: 22,
+    }
+    res.send(exampleData);
+}) 
+
+app.post('/saveData', (req, res) => {
+    console.log(req.body);
+    res.send(`data received with id: ${req.body.id}`)
 })
 
 app.listen(port, () => {
