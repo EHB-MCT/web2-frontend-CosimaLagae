633858b18471ca1754c90e69eb554b8cf9d9e80b
CosimaLagae <cosima.lagae@student.ehb.be>
2021-12-11 13:25:01 +0100

    npm install

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiY29zaW1hcm9zaWUiLCJhIjoiY2t1d3BtYnF4MDZrcTJucXFoNGN1M3hkaCJ9.RgMCkP3mQoUKWG2O5Rw4Fw';
 
-navigator.geolocation.getCurrentPosition(succesLocation, errorLocation, {
-    enableHighAccuracy: true
-})
+window.onload = () => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY29zaW1hcm9zaWUiLCJhIjoiY2t1d3BtYnF4MDZrcTJucXFoNGN1M3hkaCJ9.RgMCkP3mQoUKWG2O5Rw4Fw';
 
-function succesLocation(position){
-    console.log(position)        
+    navigator.geolocation.getCurrentPosition(succesLocation, errorLocation, {
+        enableHighAccuracy: true
+    })
+    
+    function succesLocation(position){
+        console.log(position)        
+    }
+    
+    function errorLocation(){
+    
+    }
+    
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11'
+    });
+    
+
 }
-
-function errorLocation(){
-
-}
-
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-});
