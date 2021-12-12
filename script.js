58d02eef0b7e8afeac3725fcd844108958501bce
CosimaLagae <cosima.lagae@student.ehb.be>
2021-12-12 14:45:11 +0100

    Navigation and current location added

--- script.js
+++ script.js
@@ -7,17 +7,38 @@ window.onload = () => {
     })
     
     function succesLocation(position){
-        console.log(position)        
+        console.log(position) 
+        setupMap([position.coords.longitude, position.coords.latitude])       
     }
     
     function errorLocation(){
-    
+        setupMap([4.34878, 50.85045])
     }
     
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11'
-    });
+    function setupMap(center){
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: center,
+            zoom: 14,
+        });    
+
+        const nav = new mapboxgl.NavigationControl();
+        map.addControl(nav);
+
+        var directions = new MapboxDirections({
+            accessToken: mapboxgl.accessToken,
+
+        });
+          
+
+        map.addControl(directions, 'top-left');
+
+        
+        
+    }
+
+
     
 
 }
