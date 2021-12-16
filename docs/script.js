335d75c946e43bd05f9e716ea1f22f9e0c6fb2ea
CosimaLagae <cosima.lagae@student.ehb.be>
2021-12-16 01:29:42 +0100

    Delete webpack
    webpack made it for me impossible to see the changes i made while i was coding. no console.logs were showing.. so i put it in the bin.

--- docs/script.js
+++ docs/script.js
@@ -8,7 +8,8 @@ window.onload = () => {
     
     function succesLocation(position){
         console.log(position) 
-        setupMap([position.coords.longitude, position.coords.latitude])       
+        setupMap([position.coords.longitude, position.coords.latitude])    
+   
     }
     
     function errorLocation(){
@@ -28,16 +29,32 @@ window.onload = () => {
 
         var directions = new MapboxDirections({
             accessToken: mapboxgl.accessToken,
-        });    
+        });  
+
+        map.on('click', (event) => {
+            const features = map.queryRenderedFeatures(event.point, {
+            layers: ['nightshops']
+            });
+            if (!features.length) {
+            return;
+            }
+            const feature = features[0];
+    
+            const popup = new mapboxgl.Popup({ offset: [0, -15] })
+            .setLngLat(feature.geometry.coordinates)
+            .setHTML(
+            `<h3>${feature.properties.name}</h3><p>${feature.properties.adress}</p>`
+            )
+            .addTo(map);
+        }); 
     }
 
     function getData(data){
         fetch("https://api.mapbox.com/datasets/v1/cosimarosie/ckx5z09s606qi28pjuwu8eqq5/features?access_token=pk.eyJ1IjoiY29zaW1hcm9zaWUiLCJhIjoiY2t1d3BtYnF4MDZrcTJucXFoNGN1M3hkaCJ9.RgMCkP3mQoUKWG2O5Rw4Fw")
         .then(response => response.json())
         .then(data => console.log(data.features));
-    }
-    //dataset id: ckx5z09s606qi28pjuwu8eqq5
-
-    
+        
+    }  
       
 }
+
