d5cc2e36abaaccdb55769a3a99f52ca80f390a67
CosimaLagae <cosima.lagae@student.ehb.be>
2021-12-19 23:29:45 +0100

    Wip api post

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,6 @@
-const { response } = require("express");
+/*const { response } = require("express");
 
 window.onload = () => {
-
     async function getData(){
         let response = await fetch('https://api.mapbox.com/datasets/v1/cosimarosie/ckx5z09s606qi28pjuwu8eqq5/features?access_token=pk.eyJ1IjoiY29zaW1hcm9zaWUiLCJhIjoiY2t1d3BtYnF4MDZrcTJucXFoNGN1M3hkaCJ9.RgMCkP3mQoUKWG2O5Rw4Fw',{
             method: 'post',
@@ -19,5 +18,4 @@ window.onload = () => {
     function addDataToDom(data){
         document.getElementById(form).innerText = JSON.stringify(data);
     }
-
-}
+}*/
