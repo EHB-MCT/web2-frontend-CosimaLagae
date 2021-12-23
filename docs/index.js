2c685808c8b5cbb6b5be71cfe45e9b7b147daac6
CosimaLagae <cosima.lagae@student.ehb.be>
2021-12-23 22:59:43 +0100

    WIP form post

--- docs/index.js
+++ docs/index.js
@@ -2,10 +2,10 @@
 
 window.onload = () => {
     async function getData(){
-        let response = await fetch('https://api.mapbox.com/datasets/v1/cosimarosie/ckx5z09s606qi28pjuwu8eqq5/features?access_token=pk.eyJ1IjoiY29zaW1hcm9zaWUiLCJhIjoiY2t1d3BtYnF4MDZrcTJucXFoNGN1M3hkaCJ9.RgMCkP3mQoUKWG2O5Rw4Fw',{
-            method: 'post',
-            body: JSON.stringify()
-        });
+        let response = await fetch('https://web2-nightshops.herokuapp.com/saveNightshop', {
+            method: 'POST',
+            body: new FormData(formElem)
+          });
         if (response.status == 200){
             return await response.json();
         }else{
@@ -18,4 +18,28 @@ window.onload = () => {
     function addDataToDom(data){
         document.getElementById(form).innerText = JSON.stringify(data);
     }
-}*/
+}
+
+https://web2-nightshops.herokuapp.com/saveNightshop
+
+
+        window.onload = () => {
+        async function getData(){
+            let response = await fetch('https://web2-nightshops.herokuapp.com/saveNightshop', {
+                method: 'POST',
+                body: new FormData(formElem)
+            });
+            if (response.status == 200){
+                return await response.json();
+            }else{
+                console.log('An error has occured')
+            }
+        }
+        getData().then(data => {
+            addDataToDom(data);
+        })
+        function addDataToDom(data){
+            document.getElementById(form).innerText = JSON.stringify(data);
+        }
+
+*/
